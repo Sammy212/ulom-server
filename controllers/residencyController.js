dd2424309c154cf8fd9b0aac2df66f340b5e3415
Sammy212 <samuelafo.212@gmail.com>
2024-03-01 09:36:53 +0100

    Listings Route

--- controllers/residencyController.js
+++ controllers/residencyController.js
@@ -3,6 +3,7 @@ import asyncHandler from "express-async-handler";
 import { prisma } from "../config/prismaConfig.js";
 
 
+// Add Listings to dataBase
 export const createResidency = asyncHandler(async (req, res) => {
     const { 
         title, 
@@ -42,6 +43,7 @@ export const createResidency = asyncHandler(async (req, res) => {
     }
 });
 
+// Get all the listings in dataBase
 export const getAllResidencies = asyncHandler(async(req, res) => {
     const residencies = await prisma.residency.findMany({
         orderBy: {
@@ -49,4 +51,19 @@ export const getAllResidencies = asyncHandler(async(req, res) => {
         },
     });
     res.send(residencies);
+});
+
+// Get Specific Listing
+
+export const getResidency = asyncHandler(async (req, res) => {
+    const {id} = req.params;
+
+    try{
+        const residency = await prisma.residency.findUnique({
+            where: {id: id},
+        })
+        res.send(residency);
+    }catch(err) {
+        throw new Error(err.message);
+    }
 });
--- routes/residencyRoute.js
+++ routes/residencyRoute.js
@@ -1,8 +1,9 @@
 import express from "express";
-import { createResidency, getAllResidencies } from "../controllers/residencyController.js";
+import { createResidency, getAllResidencies, getResidency } from "../controllers/residencyController.js";
 const router = express.Router();
 
 router.post("/create", createResidency);
 router.get("/all-listings", getAllResidencies);
+router.get("/:id", getResidency);
 
 export {router as residencyRoute}
